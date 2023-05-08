508ea409f311a3a369360c7ca04aebffc609263a
Jessthesaver <jesuscarmona102@gmail.com>
2023-05-08 12:24:26 -0600

    Exercise 3 init commit

---
+++ Promises/Promises_3/Exercise3.js
@@ -0,0 +1,14 @@
+const fetch = require("node-fetch");
+
+const beginFetching = (request, opts) => {
+  const controller = new AbortController();
+  const signal = controller.signal;
+  let result = {};
+
+  result = fetch(request, { ...opts, signal });
+
+  result.abort = () => controller.abort();
+  return result;
+};
+
+module.exports = beginFetching;
---
+++ Promises/Promises_3/Exercise3.test.js
@@ -0,0 +1,19 @@
+const beginFetching = require("./Exercise3");
+const fetch = require("node-fetch");
+
+const url = "https://jsonplaceholder.typicode.com/todos/1";
+
+test("Fetching works", async () => {
+  const result = await beginFetching(url);
+
+  expect(result).toBeTruthy();
+});
+
+test("Fetching aborted", async () => {
+  const result = beginFetching(url, true);
+  await result.abort();
+
+  result.catch((error) => {
+    expect(error).toHaveProperty("name", "AbortError");
+  });
+});
