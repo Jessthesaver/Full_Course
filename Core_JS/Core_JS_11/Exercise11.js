bdae1a6944405f14faee6d309fbca6891a6745b9
Jessthesaver <jesuscarmona102@gmail.com>
2023-05-16 13:09:05 -0600

    11

---
+++ Core_JS/Core_JS_11/Exercise11.js
@@ -0,0 +1,22 @@
+//Write an enhanced DOM querySelectAll method that can support the following selector:
+//Select nodes whose children match a pattern. Must select the direct parent of the child selector.
+
+function querySelectorAll(selector) {
+  const output = [];
+
+  const [parentSelector, childSelector] = selector.split("<");
+
+  let parents = document.querySelectorAll(parentSelector);
+
+  for (const parent of parents) {
+    const child = parent.querySelectorAll(childSelector);
+
+    if (child.length > 0) {
+      output.push(parent);
+    }
+  }
+
+  return output;
+}
+
+module.exports = { querySelectorAll };
--- Core_JS/Core_JS_11/Exercise11.test.js
+++ Core_JS/Core_JS_11/Exercise11.test.js
@@ -1,30 +1,30 @@
-const querySelectorAll = require("./Exercise11");
+/** @jest-environment jsdom */
+
+const { querySelectorAll } = require("./Exercise11");
 
 document.body.innerHTML = `
-<section>
-    <div id="1" class="note"><input type="checkbox" class="is-complete" checked> </div>
-    <div id="2" class="note"></div>
-    <div id="3" class="note"><input type="checkbox" class="is-complete" checked></div>
-    <div id="4" class="note"></div>
-    <div id="5" class="note"><input type="checkbox" class="is-complete" checked></div>
-    <div id="6" class="note"><input type="checkbox" class="is-complete"></div>
-    <div class="another"></div>
-    <div></div>
-</section>
+    <section>
+        <div id="1" class="note"><input type="checkbox" class="is-complete" checked> </div>
+        <div id="2" class="note"></div>
+        <div id="3" class="note"><input type="checkbox" class="is-complete" checked></div>
+        <div id="4" class="note"></div>
+        <div id="5" class="note"><input type="checkbox" class="is-complete" checked></div>
+        <div id="6" class="note"><input type="checkbox" class="is-complete"></div>
+        <div class="another"></div>
+        <div></div>
+    </section>
 `;
 
-test("Initial test", () => {
+test("initial test", () => {
   const output = [];
+  const nodeList = querySelectorAll("div.note < input.is-complete[checked]");
 
-  const nodes = querySelectorAll("div.note < input.is-complete[checked]");
-
-  for (const element of nodes) {
+  for (const element of nodeList) {
     output.push(element.innerHTML);
   }
 
-  console.log(output);
   expect(output).toEqual([
-    '<input type="checkbox" class="is-complete" checked="">',
+    '<input type="checkbox" class="is-complete" checked=""> ',
     '<input type="checkbox" class="is-complete" checked="">',
     '<input type="checkbox" class="is-complete" checked="">',
   ]);
