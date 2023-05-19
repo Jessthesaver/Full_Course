aa36d7155295c2ab11417cb010c97533aa19330d
Jessthesaver <jesuscarmona102@gmail.com>
2023-05-19 16:07:54 -0600

    18

---
+++ Core_JS/Core_JS_18/Exercise_18.test.js
@@ -0,0 +1,24 @@
+const set = require("./Exercise_18");
+
+test.skip("Basic test", () => {
+  const obj = {
+    name: "Mario",
+    age: 30,
+  };
+
+  const newObj = set(obj, "path.to.deeply.nested.property", 42);
+  console.log(newObj);
+  expect(newObj).toMatchObject({
+    name: "Mario",
+    age: 30,
+    path: {
+      to: {
+        deeply: {
+          nested: {
+            property: 42,
+          },
+        },
+      },
+    },
+  });
+});
